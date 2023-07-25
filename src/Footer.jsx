ee5321a908018696b12e16db142b49be2d9cce06
AmirMohammadHemmati <20amhs00@gmail.com>
2023-07-26 02:33:50 +0330

    update git ignore

---
+++ src/Footer.jsx
@@ -0,0 +1,38 @@
+import Mp3Player from "./Mp3Player";
+import PropTypes from "prop-types";
+Footer.propTypes = {
+  musicIsPlay: PropTypes.bool,
+  currentMusicData: PropTypes.object,
+  dispatch: PropTypes.func,
+  handlePlayButton: PropTypes.func,
+  audioRef: PropTypes.object,
+  togglePlayPause: PropTypes.func,
+  musicIndex: PropTypes.number,
+  playMusic: PropTypes.func,
+  isMute: PropTypes.bool,
+};
+export default function Footer({
+  musicIsPlay,
+  currentMusicData,
+  dispatch,
+  audioRef,
+  togglePlayPause,
+  musicIndex,
+  playMusic,
+  isMute,
+}) {
+  return (
+    <footer className="max-w-[1360px] fixed bottom-0 w-full h-auto z-20">
+      <Mp3Player
+        musicIsPlay={musicIsPlay}
+        currentMusicData={currentMusicData}
+        audioRef={audioRef}
+        togglePlayPause={togglePlayPause}
+        dispatch={dispatch}
+        musicIndex={musicIndex}
+        playMusic={playMusic}
+        isMute={isMute}
+      />
+    </footer>
+  );
+}
